f63e5bd3b95e60ba5d495ab68645f45c81a86b9b
Deesus <d.reddy@yandex.com>
2018-11-07 01:06:29 -0500

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="layout">
+        <ks-header>Menu | meow | oink | moo | woof</ks-header>
+
+        <div class="layout__content">
+            <!-- side panel: -->
+            <ks-side-nav></ks-side-nav>
+
+            <!-- main content: -->
+            <ks-main></ks-main>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    import KsSideNav from './components/KsSideNav.vue';
+    import KsMain from './components/KsMain.vue';
+    import KsHeader from './components/KsHeader.vue';
+
+    export default {
+        name: 'App',
+
+        data() {
+            return {
+            };
+        },
+
+        methods: {},
+
+        components: {
+            'ks-side-nav': KsSideNav,
+            'ks-main': KsMain,
+            'ks-header': KsHeader
+        }
+    }
+</script>
+
+
+<style scoped>
+    .layout__content {
+        margin-top: 60px;
+    }
+</style>
---
+++ src/index.js
@@ -0,0 +1,21 @@
+// N.b. we can use Node-style modules (`require`) instead of ES6 modules as well:
+// TODO: clean up imports - do we even need some of these (e.g. index.html import)?
+import 'babel-runtime/regenerator';
+import 'webpack-hot-middleware/client?reload=true';         // creates websocket connection (and reloading on change)
+import './styles/normalize.css';
+import './styles/baseKitchenSink.css';
+import './index.html';
+
+import Vue from 'vue';
+import App from './App.vue';
+
+// ==================================================
+// vue instance:
+// ==================================================
+new Vue({
+    el: '#app',
+
+    render(h) {
+        return h(App);
+    }
+});
